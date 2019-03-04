8e238d645968263a02a0b9a723d41bd073c76f38
root <root@lpicourse.fedora>
2019-03-05 01:44:50 +0200

    added ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'yakir bst'
+my_age = 26 # and getting older
+my_height = 180 # cm
+my_weight = 67 # kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} cm tall"
+puts "He's #{my_weight} kg heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffe"
+
+# this line is tricky, try to get it exactly right
+
+puts "If I add #{my_age}, #{my_height} and #{my_weight} I get #{my_age + my_height + my_weight}"
